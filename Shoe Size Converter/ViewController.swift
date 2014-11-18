bdd536ff251437b55628dbb9469a512e4a6d37e7
Amy Holms <amy@formfireglassworks.com>
2014-11-17 17:17:19 -0800

    Initial Commit

---
+++ Shoe Size Converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shoe Size Converter
+//
+//  Created by Amy Holms on 11/17/14.
+//  Copyright (c) 2014 FormFire Develops. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
